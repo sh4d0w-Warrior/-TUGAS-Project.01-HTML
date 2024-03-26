5e7ae536b5ba5c82abcf44e0bf25f33fa90823f8
hanrinov <hanrinov0001@gmail.com>
2024-03-26 15:40:02 +0800

    Memperbaiki kesalahan, menambahkan file about.html, membuat halaman about (tentang saya), membuat bagian css dari halaman about.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 
 const navbarNav = document.querySelector(".navbar-nav");
 
-// ketika humberger menu di klik
+// ketika humburger menu di klik
 
-document.querySelector("#humburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// Klik di luar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
