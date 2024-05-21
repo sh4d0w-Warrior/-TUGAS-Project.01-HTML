34f24e71e5d3fb8f906790bfa118fb79ada92f68
hanrinov <hanrinov0001@gmail.com>
2024-05-21 22:00:50 +0800

    mebuat file navbar.php dan memasukkan script untuk membuat navbar dalam file navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<!-- Navbar start -->
+<nav class="navbar">
+      <a href="#" class="navbar-logo">personal<span>homepage</span></a>
+      <div class="navbar-nav">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="blog.php">Blog</a>
+        <a href="galery.php">Gallery</a>
+        <a href="kontak.php">Contact</a>
+      </div>
+
+      <div class="navbar-extra">
+        <a href="#" id="search"><i data-feather="search"></i></a>
+        <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+      </div>
+    </nav>
+    <!-- Navbar End -->
